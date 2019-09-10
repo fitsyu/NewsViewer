d5735e929fd0e9de19e1764b21f37e3ef10d1aa1
Fitsyu Two <fitsyu2@gmail.com>
2019-09-10 15:13:43 +0700

    create base Coordinator

---
+++ NewsViewer/Coordinator/Coordinator.swift
@@ -0,0 +1,58 @@
+//
+//  Coordinator.swift
+//  NewsViewer
+//
+//  Created by Fitsyu  on 10/09/19.
+//  Copyright © 2019 Fitsyu . All rights reserved.
+//
+
+
+import UIKit
+
+// Coordinator
+protocol Coordinator {
+    
+    var presentee: UIViewController? { get }
+    
+    /**
+     load and navigate the non-Graphical UI
+     */
+    func start()
+    
+    func start(from top: UIViewController?)
+    
+    
+    /**
+     load the viewController
+     
+     create a tabItem for it
+     
+     add the ViewConttoler to the UITabControoler's children
+     */
+    func start(from top: UITabBarController)
+    
+    /**
+     load the viewController
+     and set it as the rootViewController for UIWindow
+     make the UIWindow as the KeyWindow and Visible
+     */
+    func start(from top: UIWindow)
+    
+    func end(_ completion: (()->Void)?)
+}
+
+extension Coordinator {
+    
+    func end(_ completion: (()->Void)? = nil) {
+        
+        if let navigationController = presentee?.navigationController {
+            navigationController.popViewController(animated: true)
+            completion?()
+            return
+        }
+        
+        presentee?.dismiss(animated: true, completion: {
+            completion?()
+        })
+    }
+}
